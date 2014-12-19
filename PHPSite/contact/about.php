ae29e00c80c45b299db61d7a03189efab74604f2
quixo33 <m.renaudon@gmail.com>
2014-12-19 11:58:28 +0100

    modif visuel

--- PHPSite/contact/about.php
+++ PHPSite/contact/about.php
@@ -90,18 +90,6 @@
     <hr class="featurette-divider">
 
     <div class="row featurette">
-        <div class="col-md-5">
-            <img class="featurette-image img-responsive" src="../img/test.png" alt="Generic placeholder image">
-        </div>
-        <div class="col-md-7">
-            <h2 class="featurette-heading">Oh yeah, it's that good. <span class="text-muted">See for yourself.</span></h2>
-            <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-    </div>
-
-    <hr class="featurette-divider">
-
-    <div class="row featurette">
         <div class="col-md-7">
             <h2 class="featurette-heading">And lastly, this one. <span class="text-muted">Checkmate.</span></h2>
             <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
